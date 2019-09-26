aad44a1d1c2dabee45204537740b15e871d121d6
周哲超 <zhouzhechao@meituan.com>
2019-09-26 10:01:43 +0800

    fix:fix imported component  undefined

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ RaScrolltop.install = function (Vue) {
     Vue.component(RaScrolltop.name, RaScrolltop)
 }
 
+
 /* supports the use of tags to introduce */
-if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.component(RaScrolltop.name, RaScrolltop)
-}
+// if (typeof window !== 'undefined' && window.Vue) {
+//     window.Vue.component(RaScrolltop.name, RaScrolltop)
+// }
 
 export default RaScrolltop
